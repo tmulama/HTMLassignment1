d2c580631d23f89431fb9cee5439ae8e7ca00eab
Tanah <mulamatanah@gmail.com>
2024-02-25 18:56:03 +0300

    second commit

---
+++ user_input.py
@@ -0,0 +1,8 @@
+name = input('What is your name:  ')
+age = input('How old are you:  ')
+location = input('Where do you live:  ')
+
+print("Hello " + name , end="")
+print(" you are " + age, end="")
+print(" years old and live in " + location)
+
